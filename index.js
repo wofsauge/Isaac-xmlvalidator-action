398c887232d18b6b964f83f05e7785f966ef5907
Wofsauge <jan-.-@t-online.de>
2022-10-13 22:17:14 +0200

    fix root folder

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const core = require('@actions/core');
 const glob = require('@actions/glob');
 
 async function handleFiles() {
-  const globber = await glob.create(rootFolder+'/.xml')
-  for await (const file of globber.globGenerator()) {
-    console.log(file);
-  }
-}
-
-try {
   const rootFolder = core.getInput('root-folder');
   if (!rootFolder) {
     console.log(`No Root folder provided. Using whole repository.`);
@@ -17,6 +10,13 @@ try {
     console.log(`Root folder provided: ${rootFolder}!`);
   }
 
+  const globber = await glob.create(rootFolder+'/.xml')
+  for await (const file of globber.globGenerator()) {
+    console.log(file);
+  }
+}
+
+try {
   handleFiles();
 
 } catch (error) {
