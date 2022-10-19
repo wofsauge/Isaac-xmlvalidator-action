ce83caf8081f75effdb1dff581c6c43fffdd88a0
Wofsauge <jan-.-@t-online.de>
2022-10-19 21:43:14 +0200

    Yannick fixes

--- index.js
+++ index.js
@@ -21,100 +21,81 @@ async function handleFiles() {
   rootFolder = "vanillaFiles";
   const globber = await glob.create(rootFolder + "/**.xml");
   const files = await globber.glob();
-  console.log(
-    `Found ` + files.length + ` files inside root folder: ${rootFolder}`
-  );
+  console.log(`Found ` + files.length + ` files inside root folder: ${rootFolder}`);
 
-  JSDOM.fromURL("https://wofsauge.github.io/Isaac-XML-Validator/isaacTypes.xsd")
-    .then((dom) => {
-      refXML = dom;
-      files.forEach((file) => {
-        var filename = file.replace(/^.*[\\\/]/, "");
-        if (fileIgnoreList.indexOf(filename) === -1) {
-          var xmlFileContent;
-          JSDOM.fromFile(file)
-            .then((dom) => {
-              xmlFileContent = dom.serialize();
-              loadXSDFile(filename, xmlFileContent, file);
-            })
-            .catch((e) => {
-              console.log(file);
-              console.log(e);
-            });
-        }
-      });
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+  refXML = await JSDOM.fromURL("https://wofsauge.github.io/Isaac-XML-Validator/isaacTypes.xsd");
+  for (const file of files) {
+    var filename = file.replace(/^.*[\\\/]/, "");
+    if (fileIgnoreList.indexOf(filename) === -1) {
+      var xmlFileContent;
+      try {
+        var dom = await JSDOM.fromFile(file);
+        xmlFileContent = dom.serialize();
+        await loadXSDFile(filename, xmlFileContent, file);
+      } catch (error) {
+        console.log(file);
+        console.log(error);
+      }
+    }
+  }
 }
 
-function loadXSDFile(fileName, xmlFileContent, file) {
+async function loadXSDFile(fileName, xmlFileContent, file) {
   if (!fileName.trim()) {
     return; // is empty
   }
   fileName = fileName.toLowerCase();
   fileName = fileName.replace(".xml", ".xsd");
+  var dom = await JSDOM.fromURL("https://wofsauge.github.io/Isaac-XML-Validator/xsd/" + fileName);
+  var xmlFile = dom.window.document;
+  const refXMLCopy = JSDOM.fragment(refXML.serialize());
+  // Workaround for HTTPS imports not working with this xsd validator library
+  // manually import the file content
 
-  JSDOM.fromURL(
-    "https://wofsauge.github.io/Isaac-XML-Validator/xsd/" + fileName
-  )
-    .then((dom) => {
-      var xmlFile = dom.window.document;
-      const refXMLCopy = JSDOM.fragment(refXML.serialize());
-      // Workaround for HTTPS imports not working with this xsd validator library
-      // manually import the file content
+  if (xmlFile.firstChild.attributes && xmlFile.firstChild.attributes["xmlns:xsisaac"]) xmlFile.firstChild.attributes.removeNamedItem("xmlns:xsisaac");
 
-      if (xmlFile.firstChild.attributes && xmlFile.firstChild.attributes["xmlns:xsisaac"])
-        xmlFile.firstChild.attributes.removeNamedItem("xmlns:xsisaac");
-
-      for (let c of xmlFile.firstChild.childNodes) {
-        if (c.nodeName == "xs:import") {
-          for (let importNode of refXMLCopy.firstChild.childNodes) {
-            if (importNode.nodeName != "#text") {
-              c.parentNode.insertBefore(importNode, c);
-            }
-          }
-          c.parentNode.removeChild(c);
-          break;
+  for (let c of xmlFile.firstChild.childNodes) {
+    if (c.nodeName == "xs:import") {
+      for (let importNode of refXMLCopy.firstChild.childNodes) {
+        if (importNode.nodeName != "#text") {
+          c.parentNode.insertBefore(importNode, c);
         }
       }
+      c.parentNode.removeChild(c);
+      break;
+    }
+  }
 
-      schemaData = dom.serialize();
-      schemaData = schemaData.replaceAll("xsisaac:", ""); // replace schema identifier, which no longer is needed due to manual import
-      schemaData = schemaData.replaceAll("simpletype", "simpleType"); // fix dom.serialize turning the attribute into lowercase...
-      schemaFileName = fileName;
+  schemaData = dom.serialize();
+  schemaData = schemaData.replaceAll("xsisaac:", ""); // replace schema identifier, which no longer is needed due to manual import
+  schemaData = schemaData.replaceAll("simpletype", "simpleType"); // fix dom.serialize turning the attribute into lowercase...
+  schemaFileName = fileName;
 
-      var Module = {
-        xml: xmlFileContent,
-        schema: schemaData,
-        arguments: [
-          "--noout",
-          "--schema",
-          schemaFileName,
-          schemaFileName.replace(".xsd", ".xml"),
-        ],
-      };
-      console.log("Now analyzing:" + file);
+  var Module = {
+    xml: xmlFileContent,
+    schema: schemaData,
+    arguments: ["--noout", "--schema", schemaFileName, schemaFileName.replace(".xsd", ".xml")],
+  };
+  console.log("Now analyzing:" + file);
+  try {
+    var result = xmllint.validateXML(Module);
+  } catch (error) {
+    console.error(error);
+  }
 
-      var result = xmllint.validateXML(Module);
-
-      if (result.errors !== null) {
-        result.errors.forEach((element) => {
-          console.error(element);
-        });
-      } else {
-        console.log("Success");
-      }
-    })
-    .catch((e) => {
-      console.error(file);
-      console.error(e);
+  if (result.errors !== null) {
+    result.errors.forEach((element) => {
+      console.error(element);
     });
+  } else {
+    console.log("Success");
+  }
 }
 
-try {
-  handleFiles();
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  try {
+    await handleFiles();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
