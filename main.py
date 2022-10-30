1443fb5103cbb4a4fa79789bfeb2816f2545b70c
Wofsauge <jan-.-@t-online.de>
2022-10-31 00:25:21 +0100

    Colorized prints and try fix code errors

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import os
+import sys
 import glob
 import lxml
-import xml.etree.ElementTree as ET
+import lxml.etree
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 fileIgnoreList = ["seedmenu.xml", "fxlayers.xml"]
 
 def clearIsaacRefsRecursive(node):
-    for child in node.getchildren():
+    for child in list(node):
         if child.get("type") is not None:
             child.set("type",child.get("type").replace("xsisaac:",""))
         clearIsaacRefsRecursive(child)
+
+def printErr(string):
+    print(bcolors.FAIL + str(string) + bcolors.ENDC)
+
+def printOK(string):
+    print(bcolors.OKGREEN + str(string) + bcolors.ENDC)
+
+def printWarn(string):
+    print(bcolors.WARNING + str(string) + bcolors.ENDC)
 
 def main():
     rootFolder = "**"
@@ -31,36 +51,42 @@ def main():
 
         errCount = 0
         try:
-            xmlschema_root_doc = ET.parse("Isaac-XML-Validator/isaacTypes.xsd")
-            xmlschema_doc = ET.parse("Isaac-XML-Validator/xsd/" + filteredFilename.replace(".xml", ".xsd"))
+            xmlschema_root_doc = lxml.etree.parse("Isaac-XML-Validator/isaacTypes.xsd")
+            xmlschema_doc = lxml.etree.parse("Isaac-XML-Validator/xsd/" + filteredFilename.replace(".xml", ".xsd"))
 
             #Replace import node with content of the imported file, because lxml doesnt like https links 
             node = xmlschema_doc.getroot().find("{http://www.w3.org/2001/XMLSchema}import")
             if node is not None:
-                for child in xmlschema_root_doc.getroot().getchildren():
+                for child in list(xmlschema_root_doc.getroot()):
                     xmlschema_doc.getroot().insert(0,child)
                 xmlschema_doc.getroot().remove(node)
             clearIsaacRefsRecursive(xmlschema_doc.getroot())
-            xmlschema = ET.XMLSchema(xmlschema_doc)
+            xmlschema = lxml.etree.XMLSchema(xmlschema_doc)
 
-            xml_doc = ET.parse(filename)
+            xml_doc = lxml.etree.parse(filename)
             isValid = xmlschema.validate(xml_doc)
             if not isValid:
                 for error in xmlschema.error_log:
-                    print(error.filename+":line "+str(error.line)+":col "+str(error.column)+": "+error.message)
+                    printErr(error.filename+":line "+str(error.line)+":col "+str(error.column)+": "+error.message)
+                errCount += len(xmlschema.error_log)
             else:
-                print("File is valid")
+                printOK("File is valid")
         except Exception as err:
-            print(err)
+            printErr(err)
             errCount += 1
-            print("SYNTAX ERROR DETECTED!!")
+            printErr("SYNTAX ERROR DETECTED!!")
 
         if errCount > 0:
             print("---- End errors for file: " + filename)
 
         totalErrorCount += errCount
     print("~~~~~ Finished analysing " + str(len(files)) + " files! ~~~~~")
-    print("Found: " + str(totalErrorCount) + " Errors")
+    if totalErrorCount >0:
+        printErr("Found: " + str(totalErrorCount) + " Errors")
+    else:
+        printOK("No errors found")
+    
+    sys.exit(totalErrorCount > 0)
 
 
 if __name__ == "__main__":
